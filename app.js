a1f31e38f94b3302dbbc2083c318b8b3a11c1249
NahlaSultan <46445279+NahlaSultan@users.noreply.github.com>
2020-12-15 22:43:40 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+//const users_routes=require('./routes/user_routes')
+const app = express()
+const jwt=require("jsonwebtoken")
+require("dotenv").config()
+//access body of request
+//so that body of the request isn't undefined
+app.use(express.json())
+//lama ted5ol gowa roh 3ala /register
+//app.use('',users_routes)
+const staff_member_routes = require('./routes/staff_member_routes')
+app.use('',staff_member_routes )
+
+module.exports.app = app
---
+++ index.js
@@ -0,0 +1,10 @@
+const {app} = require('./app.js')
+const staff_member_routes=require('./routes/staff_member_routes')
+const mongoose = require('mongoose')
+require('dotenv').config()
+//console.log(process.env.DB_URL)
+
+staff_member_routes.route('')
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true , useUnifiedTopology:true})
+
+app.listen(process.env.PORT)
