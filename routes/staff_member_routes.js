f5b5c62ec36c9d30ca349aee0739957be011d489
Nahla <nahlarsultan@gmail.com>
2020-12-16 09:50:30 +0200

    token trial2

--- routes/staff_member_routes.js
+++ routes/staff_member_routes.js
@@ -30,6 +30,31 @@ router.route('/login')
     return res.status(401).send('Invalid email')
 })
 
+const auth = (req,res,next)=>{
+    try{
+             const token=req.header('token');
+             
+ 
+             const verified = jwt.verify(token,process.env.TOKEN_SECRET);
+             console.log(verified);
+             if(!verified){
+                 return res.status(401).json({msg:"authorization failed"});
+             }
+             req.user=verified._id;
+             next();
+    }
+    catch(error){
+     res.status(500).json({error:error.message});
+ 
+    }
+ }
+router.route('/homePage')
+.get(auth,(req,res)=>{
+
+    res.send('/home')
+})
+
+
 router.route('/signOut')
 .get((req,res)=>{
     res.send('/login')
